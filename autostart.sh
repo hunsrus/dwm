e56b24b6143823fa39595a673403d5d0f809cb76
hunsrus <gabie00018@gmail.com>
2023-02-18 09:44:47 -0300

    change background

--- autostart.sh
+++ autostart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 picom -b --config /home/gabriel/.config/picom/picom.conf
 dwmblocks &
-feh --bg-scale /home/gabriel/Imágenes/Fondos\ de\ escritorio/gradiente_bn.png &
+feh --bg-scale '/home/gabriel/Imágenes/Fondos de escritorio/fabrizio-conti-sK7own3aaw-unsplash.jpg' &
 /usr/lib/kdeconnectd &
 dunst &
 xbindkeys
