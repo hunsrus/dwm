42d6efa2d43f587e2e6d2517802a70f6c6b19c22
hunsrus <gabie00018@gmail.com>
2023-04-26 14:28:56 -0300

    mount cloud storage

--- autostart.sh
+++ autostart.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-picom -b --config /home/gabriel/.config/picom/picom.conf
+picom -b --config /home/gabriel/.config/picom/picom-jonaburg.conf --experimental-backends
 dwmblocks &
-feh --bg-scale '/home/gabriel/Imágenes/Fondos de escritorio/fabrizio-conti-sK7own3aaw-unsplash.jpg' &
+feh --bg-scale '/home/gabriel/Imágenes/Wallpapers/fabrizio-conti-sK7own3aaw-unsplash.jpg' &
 /usr/lib/kdeconnectd &
 dunst &
+rclone --vfs-cache-mode writes mount onedrive: ~/OneDrive &
+rclone --vfs-cache-mode writes mount googledrive: ~/Drive &
+rclone --vfs-cache-mode writes mount mega: ~/Mega &
 xbindkeys
