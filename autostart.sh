2dbd744516834b5a767bb698c0f11dd9bcff2fdc
hunsrus <gabie00018@gmail.com>
2024-01-22 19:40:04 -0300

    run polybar and change wallpaper

--- autostart.sh
+++ autostart.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 picom -b --config /home/gabriel/.config/picom/picom-jonaburg.conf --experimental-backends
-dwmblocks &
+#dwmblocks &
+/home/gabriel/.config/polybar/launch.sh &
 #feh --bg-scale '/home/gabriel/Imágenes/Wallpapers/fabrizio-conti-sK7own3aaw-unsplash.jpg' &
 #feh --bg-scale '/home/gabriel/Imágenes/Wallpapers/clouds-dwm.png' &
-feh --bg-scale '/home/gabriel/Imágenes/Wallpapers/gris.jpg' &
+#feh --bg-scale '/home/gabriel/Imágenes/Wallpapers/gris.jpg' &
+#feh --bg-scale '/home/gabriel/Imágenes/Wallpapers/222222.png' &
+feh --bg-scale '/home/gabriel/Imágenes/Wallpapers/-zgH2-pop-dark.png' &
 /usr/lib/kdeconnectd &
 dunst &
 sxhkd &
